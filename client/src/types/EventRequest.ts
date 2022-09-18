b119106617eea0e685760be0dfdcf0992c671b21
Viranga Jayawaradana <virangapasindu4@gmail.com>
2022-09-19 01:05:45 +0530

    event req type

---
+++ client/src/types/EventRequest.ts
@@ -0,0 +1,21 @@
+export default interface IEventRequest {
+    event_id: String,
+    event_type: String,
+    type: String,
+    title: String,
+    remarks: String,
+    start_date: String,
+    end_date: String,
+    start_time: String,
+    end_time: String,
+    no_participants: Number,
+    budget: Number,
+    project: String,
+    vote: String,
+    location: String,
+    venue_name: String,
+    venue_type: String,
+    fund_type: String,
+    
+
+  }
