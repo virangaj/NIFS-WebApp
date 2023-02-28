e03466b0c2f23666b76e45e0daa6aae4e779ddec
RavinduFernando <f.ravindu@gmail.com>
2023-02-28 20:45:27 +0530

    started implementing the UI for the ADMIN

--- client/src/pages/AdminMainPage.tsx
+++ client/src/pages/AdminMainPage.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import SecondaryNavbar from '../components/shared/SecondaryNavbar';
-import Pages from '../components/data/AdminNavData.json';
+import React from "react";
+import SecondaryNavbar from "../components/shared/SecondaryNavbar";
+import Pages from "../components/data/AdminNavData.json";
 
-import Dots from '../images/dots_circle_b.png';
-import { Route, Routes } from 'react-router-dom';
-import { RouteName } from '../constant/routeNames';
-import ContractExtension from './admin/ContractExtension';
+import Dots from "../images/dots_circle_b.png";
+import { Route, Routes } from "react-router-dom";
+import { RouteName } from "../constant/routeNames";
+import ContractExtension from "./admin/ContractExtension";
+import AdministrativeReport from "./admin/AdministrativeReport";
+import Accomodation from "./admin/Accomodation";
 
 function AdminMainPage() {
-	return (
-		<div className='body-content min-h-[80vh]'>
-			{/* <SeduSecondaryNavbar /> */}
-			<SecondaryNavbar pages={Pages} />
-			<div className='fixed w-[400px] top-[-100px] right-[-100px] -z-10'>
-				<img src={Dots} alt='Dots' />
-			</div>
+  return (
+    <div className="body-content min-h-[80vh]">
+      {/* <SeduSecondaryNavbar /> */}
+      <SecondaryNavbar pages={Pages} />
+      <div className="fixed w-[400px] top-[-100px] right-[-100px] -z-10">
+        <img src={Dots} alt="Dots" />
+      </div>
 
-			<Routes>
-				<Route path={RouteName.ContractExtension} element={<ContractExtension />} />
-			</Routes>
-		</div>
-	);
+      {/* <AdministrativeReport /> */}
+      <Accomodation />
+
+      <Routes>
+        <Route
+          path={RouteName.ContractExtension}
+          element={<ContractExtension />}
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default AdminMainPage;
