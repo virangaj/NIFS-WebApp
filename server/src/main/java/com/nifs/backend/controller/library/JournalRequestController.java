53ad2b26a5bdcc42a12c4cb598eb1d37663ea2f7
RavinduFernando <f.ravindu@gmail.com>
2023-04-14 14:31:12 +0530

    Library admin dashboard

--- server/src/main/java/com/nifs/backend/controller/library/JournalRequestController.java
+++ server/src/main/java/com/nifs/backend/controller/library/JournalRequestController.java
@@ -1,10 +1,15 @@
 package com.nifs.backend.controller.library;
 
+import com.nifs.backend.config.JwtService;
+import com.nifs.backend.constant.RequestStatus;
 import com.nifs.backend.dto.library.JournalRequestDTO;
 import com.nifs.backend.serviceImplementation.library.JournalRequestService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/library/journal-request")
@@ -13,18 +18,34 @@ public class JournalRequestController {
     final
     JournalRequestService journalRequestService;
 
-    public JournalRequestController(JournalRequestService journalRequestService) {
+    final
+    JwtService jwtService;
+
+    public JournalRequestController(JournalRequestService journalRequestService, JwtService jwtService) {
         this.journalRequestService = journalRequestService;
+        this.jwtService = jwtService;
     }
 
-    @GetMapping
-    public ResponseEntity<?> getAllJournalRequest(){
-        return ResponseEntity.ok("Success");
-    }
 
     @PostMapping("/add")
     public ResponseEntity<?> createNewJournalRequest(@RequestBody JournalRequestDTO data){
-        return journalRequestService.createNewJournalRequest(data);
+        return ResponseEntity.ok(journalRequestService.createNewJournalRequest(data));
+    }
 
+    @GetMapping
+    public ResponseEntity<?> getAllJournalRequests(@RequestParam(required = false) String division){
+        return ResponseEntity.ok(journalRequestService.getAllJournalRequests(division));
+    }
+
+    @PutMapping("/hod/status")
+    public ResponseEntity<?> putHodApproval(@RequestParam RequestStatus approval, @RequestBody List<String> resId, @AuthenticationPrincipal UserDetails userDetails){
+        String user = userDetails.getUsername();
+        return ResponseEntity.ok(journalRequestService.putHodApproval(approval,resId,user));
+    }
+
+    @PutMapping("/director/status")
+    public ResponseEntity<?> putDirectorApproval(@RequestParam RequestStatus approval, @RequestBody List<String> resId, @AuthenticationPrincipal UserDetails userDetails){
+        String user = userDetails.getUsername();
+        return ResponseEntity.ok(journalRequestService.putDirectorApproval(approval, resId, user));
     }
 }
