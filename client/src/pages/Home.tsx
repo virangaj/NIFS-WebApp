5fd9480a0ce4c82686cc5231061e4149eed20dc9
virangaj <=>
2023-04-15 13:33:21 +0530

    add home page routing

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from 'react';
 import Pages from '../components/data/MainNavPages.json';
 import { Link } from 'react-router-dom';
 import HomeMainNavCard from '../components/HomeMainNavCard';
 
 function Home() {
 	return (
-		<div className='body-content'>
+		<div className='flex items-center justify-center h-[750px] body-content'>
 			{Pages.map((p, i) => (
 				<Link to={p.link} key={i}>
 					<HomeMainNavCard title={p.title} img={p.img} />
