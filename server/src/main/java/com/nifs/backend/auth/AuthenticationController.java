1739b499a06a3c4ca66e7e96acb916073c94bbb6
virangaj <=>
2023-04-16 01:32:05 +0530

    login error handled

--- server/src/main/java/com/nifs/backend/auth/AuthenticationController.java
+++ server/src/main/java/com/nifs/backend/auth/AuthenticationController.java
@@ -4,11 +4,12 @@ package com.nifs.backend.auth;
 import com.nifs.backend.constant.RequestStatus;
 import com.nifs.backend.dto.auth.ResetPasswordDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+@Log4j2
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
@@ -30,8 +31,8 @@ public class AuthenticationController {
         try {
             return ResponseEntity.ok(service.loginRequest(request));
         } catch (Exception e) {
-            System.out.println(e.toString());
-            return ResponseEntity.ok(AuthenticationResponse.builder()
+            log.error("------------error : " + e.toString());
+            return ResponseEntity.badRequest().body(AuthenticationResponse.builder()
                     .status(String.valueOf(RequestStatus.UNAUTHORIZED))
                     .code(200)
                     .message("Please check your credentials!")
